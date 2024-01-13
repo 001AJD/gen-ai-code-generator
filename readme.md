5e8ca9c78a7ad5de8c224675be592d4efb26f748
Ajinkya Dhomne <ajinkyadhomne.27@gmail.com>
2024-01-13 10:25:29 +0530

    updates

--- readme.md
+++ readme.md
@@ -9,3 +9,8 @@
 -   Rename the sample.env file to .env file
 -   Replace API URL and make appropriate changes as mentioned in the [Google documentation here](https://cloud.google.com/vertex-ai/docs/generative-ai/model-reference/code-generation#sample_request)
 -   Vertex Code Generation AI API is paid service need to setup the project with active billing account
+
+### Generate Authentication Token
+
+-   gcloud auth login
+-   gcloud auth print-access-token
--- websockets/socket.js
+++ websockets/socket.js
@@ -15,8 +15,7 @@ const registerSocketEventHandlers = (wss) => {
 			let response = "";
 			console.log("message received from the client");
 			try {
-				// let result = await generateCode(message.toString());
-				let result = "op from gen ai";
+				let result = await generateCode(message.toString());
 				response = result;
 			} catch (err) {
 				response = { error: "Internal Error Occured" };
@@ -26,7 +25,6 @@ const registerSocketEventHandlers = (wss) => {
 			);
 
 			socket.send(response);
-			
 		});
 
 		// handle connection close event
